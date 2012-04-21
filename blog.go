f00309403cc7c90c17937828f46bd2cd4bbf75cf
Andrew Gallant (Ocelot) <Andrew.Gallant@tufts.edu>
2012-04-21 01:06:34 -0400

    setting up main index

--- blog.go
+++ blog.go
@@ -1,16 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+
+	"code.google.com/p/gorilla/mux"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hiya, there!")
+var (
+	view *template.Template
+)
+
+func init() {
+	view = template.New("view").ParseGlob("views/*.html")
 }
 
 func main() {
-	http.HandleFunc("/", handler)
+	r := mux.NewRouter()
+	r.HandleFunc("/", showIndex)
+
+	http.Handle("/", r)
 	http.ListenAndServe(":8081", nil)
+}
+
+func mainIndex(w http.ResponseWriter, req *http.Request) {
+	err := view.ExecuteTemplate(w, "index",
+		struct {
+			Title string
+		}{
+			Title: "Hiya :-)",
+		})
+	if err != nil {
+		panic(err)
+	}
 }
 
