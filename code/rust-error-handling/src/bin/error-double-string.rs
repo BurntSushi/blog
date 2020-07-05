9bac09b5bc39c377199fecbaa44d1de673f8bc89
Jingjing Duan <duanjingjing@gmail.com>
2020-07-05 11:43:52 -0700

    Actually double arg

--- code/rust-error-handling/src/bin/error-double-string.rs
+++ code/rust-error-handling/src/bin/error-double-string.rs
@@ -7,6 +7,7 @@ fn double_arg(mut argv: env::Args) -> Result<i32, String> {
     argv.nth(1)
         .ok_or("Please give at least one argument".to_owned())
         .and_then(|arg| arg.parse::<i32>().map_err(|err| err.to_string()))
+        .map(|i| i * 2)
 }
 
 fn main() {
